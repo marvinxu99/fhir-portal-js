5b550c41817c2b5bdd63800ecb2728b2c853280a
Marvin Xu <marvinxu99@hotmail.com>
2024-10-09 20:41:13 -0700

    ...

---
+++ fhir-portal-js/server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for the OAuth callback page
+app.get('/callback', (req, res) => {
+  res.sendFile(path.join(__dirname, 'callback.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
